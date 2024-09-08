c6df238f7cf7ab3ca8e59c06a29150592453a0fd
Adam Koprek <112286313+adkoprek@users.noreply.github.com>
2024-09-08 13:08:12 +0200

    Added favicon

---
+++ assets/js/icon_0.js
@@ -0,0 +1,11 @@
+const setFavicon = () => {
+    const favicon = document.querySelector('link[rel="icon"]');
+    favicon.href = (window.matchMedia('(prefers-color-scheme: dark)').matches)
+                    ? 'assets/drawable/logo_dark.svg'
+                    : 'assets/drawable/logo_light.svg';
+};
+
+window.matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', setFavicon)
+
+setFavicon()
